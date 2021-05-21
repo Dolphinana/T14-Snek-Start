798ee08bc5a370d6b9fff63fe2343dd8ba856f34
Dolphinana <68866995+Dolphinana@users.noreply.github.com>
2021-05-21 15:31:50 +0200

    Everything is blu

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -10,9 +10,9 @@ void Board::DrawCell(Location & loc, Color c)
 {
 	gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, dimension, dimension, c);
 
-	gfx.DrawRectDim(loc.x * dimension, loc.y, dimension,  loc.y + 1, Colors::Blue);
-	gfx.DrawRectDim(loc.x * dimension, loc.y, loc.x + 1,  dimension, Colors::Blue);
-	gfx.DrawRectDim(loc.x * dimension, loc.y, dimension,  dimension, Colors::Blue);
+	gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, dimension,  loc.y + 1, Colors::Blue);
+	gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, loc.x + 1,  dimension, Colors::Blue);
+	gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, dimension,  dimension, Colors::Blue);
 	gfx.DrawRectDim(loc.x, dimension, loc.y, loc.y + 1, Colors::Blue);
 }
 
