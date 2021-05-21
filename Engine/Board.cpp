0664923c07be5c364737dd0044118e010cc8e071
Dolphinana <68866995+Dolphinana@users.noreply.github.com>
2021-05-21 15:31:18 +0200

    More weird blue square

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -10,10 +10,10 @@ void Board::DrawCell(Location & loc, Color c)
 {
 	gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, dimension, dimension, c);
 
-	gfx.DrawRectDim(loc.x, loc.y, dimension,  loc.y + 1, Colors::Blue);
-	gfx.DrawRectDim(loc.x, loc.y, loc.x + 1,  dimension, Colors::Blue);
-	gfx.DrawRectDim(loc.x, loc.y, dimension,  dimension, Colors::Blue);
-	gfx.DrawRectDim(loc.x + dimension, dimension, loc.y, loc.y + 1, Colors::Blue);
+	gfx.DrawRectDim(loc.x * dimension, loc.y, dimension,  loc.y + 1, Colors::Blue);
+	gfx.DrawRectDim(loc.x * dimension, loc.y, loc.x + 1,  dimension, Colors::Blue);
+	gfx.DrawRectDim(loc.x * dimension, loc.y, dimension,  dimension, Colors::Blue);
+	gfx.DrawRectDim(loc.x, dimension, loc.y, loc.y + 1, Colors::Blue);
 }
 
 int Board::GetWidth()
