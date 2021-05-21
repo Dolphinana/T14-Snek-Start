8a3cc4053ae0b2f556314e8ee7887ec5555c7c6f
Dolphinana <68866995+Dolphinana@users.noreply.github.com>
2021-05-21 15:33:33 +0200

    löng snek

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -10,10 +10,10 @@ void Board::DrawCell(Location & loc, Color c)
 {
 	gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, dimension, dimension, c);
 
-	gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, dimension,  loc.y + 1, Colors::Blue);
-	gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, loc.x + 1,  dimension, Colors::Blue);
-	gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, dimension,  dimension, Colors::Blue);
-	gfx.DrawRectDim(loc.x, dimension, loc.y, loc.y + 1, Colors::Blue);
+	gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, dimension,  (loc.y * dimension) + 1, Colors::Blue);
+	//gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, loc.x + 1,  dimension, Colors::Blue);
+	//gfx.DrawRectDim(loc.x * dimension, loc.y * dimension, dimension,  dimension, Colors::Blue);
+	//gfx.DrawRectDim(loc.x, dimension, loc.y, loc.y + 1, Colors::Blue);
 }
 
 int Board::GetWidth()
